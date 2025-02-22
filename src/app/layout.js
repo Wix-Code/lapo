8a235755189a734d44dcf6c9ea98319f4d7697ca
Wix-Code <ogbonna428alex@gmail.com>
2025-02-22 06:39:35 +0100

    push

--- src/app/layout.js
+++ src/app/layout.js
@@ -40,7 +40,7 @@ export default function RootLayout({ children }) {
             <LogoutIcon />
           </div>
         </div>
-        <div className="bg-blue-50 w-[80%] sm:w-full">
+        <div className="bg-blue-50 w-[80%] sm:w-[100%">
           {children}
         </div>
       </body>
