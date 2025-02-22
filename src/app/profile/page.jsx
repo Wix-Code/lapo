6fd484ea13502d5b9e4b6bc96bbc4bba303d0ea7
Wix-Code <ogbonna428alex@gmail.com>
2025-02-22 06:46:21 +0100

    push

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -19,8 +19,8 @@ const Profile = () => {
         <span className='text-[#475467] text-[14px] font-normal'>View and attend to card requests here.</span>
         <div className='flex flex-col gap-4 w-full bg-[#FFFFFF] p-5 border-[#DEE6E2] border-[1px]'>
           <h1 className='text-[18px] text-[#101828] font-bold'>Card Request</h1>
-          <div className='flex flex-col gap-4 sm:flex sm:flex-col sm:gap-3'>
-            <div className='flex items-center gap-7'>
+          <div className='flex flex-col gap-4'>
+            <div className='flex items-center gap-7  sm:flex sm:flex-col sm:gap-3'>
               <div className='flex flex-col gap-2 w-[448px]'>
                 <label className='text-[#344054] text-[14px]' htmlFor="">Branch Name</label>
                 <input type="text" placeholder='Corperate' className=' bg-[#FFFFFF] p-2 border-[#DEE6E2] border-[1px]' />
@@ -30,7 +30,7 @@ const Profile = () => {
                 <input type="text" placeholder='RootUser' className='text-[#101828] text-[16px] bg-[#FFFFFF] p-2 border-[#DEE6E2] border-[1px]'/>
               </div>
             </div>
-            <div className='flex items-center gap-7'>
+            <div className='flex items-center gap-7  sm:flex sm:flex-col sm:gap-3'>
               <div className='flex flex-col gap-2 w-[448px]'>
                 <label className='text-[#344054] text-[14px]' htmlFor="">Card Type</label>
                 <input type="text" placeholder='Classic Debit' className=' bg-[#FFFFFF] p-2 border-[#DEE6E2] border-[1px]' />
@@ -40,7 +40,7 @@ const Profile = () => {
                 <input type="text" placeholder='₦1,500' className='text-[#101828] text-[16px] bg-[#FFFFFF] p-2 border-[#DEE6E2] border-[1px]'/>
               </div>
             </div>
-            <div className='flex items-center gap-7'>
+            <div className='flex items-center gap-7 sm:flex sm:flex-col sm:gap-3'>
               <div className='flex flex-col gap-2 w-[448px]'>
                 <label className='text-[#344054] text-[14px]' htmlFor="">Quantity</label>
                 <input type="text" placeholder='10' className=' bg-[#FFFFFF] p-2 border-[#DEE6E2] border-[1px]' />
@@ -50,7 +50,7 @@ const Profile = () => {
                 <input type="text" placeholder='847264905' className='text-[#101828] text-[16px] bg-[#FFFFFF] p-2 border-[#DEE6E2] border-[1px]'/>
               </div>
             </div>
-            <div className='flex items-center gap-7'>
+            <div className='flex items-center gap-7 sm:flex sm:flex-col sm:gap-3'>
               <div className='flex flex-col gap-2 w-[448px]'>
                 <label className='text-[#344054] text-[14px]' htmlFor="">Date Requested</label>
                 <input type="text" placeholder='Corperate' className=' bg-[#FFFFFF] p-2 border-[#DEE6E2] border-[1px]' />
@@ -62,7 +62,7 @@ const Profile = () => {
             </div>
             <div>
               <p>Actions</p>
-              <div className='flex items-center gap-6 my-2'>
+              <div className='flex items-center gap-6 my-2 sm:flex sm:flex-col sm:gap-3'>
                 <div className='flex items-center gap-2 p-3 bg-[#344054] cursor-pointer'>
                   <Image src="/pics/e.png" alt='Card Icon' height={16} width={14} />
                   <p className='text-[12px] text-[#FFFFFF]'>Download for Production</p>
