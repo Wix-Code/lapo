b218621bb770d93f9ea4c611ada5f190c608426f
Wix-Code <ogbonna428alex@gmail.com>
2025-02-25 14:35:38 +0100

    push

--- src/app/card/create-profile/page.jsx
+++ src/app/card/create-profile/page.jsx
@@ -8,56 +8,61 @@ const CardUser = () => {
   return (
     <div>
       <div className='flex items-center justify-between h-[48px] bg-[#FFFFFF] border-[#DEE6E2] border-[1px] px-5 top-0 sticky z-50'>
-        <div className='flex items-center gap-4'>
-          <Image src="/pics/ca.png" alt='Card Icon' height={16} width={16} />
-          <span className='text-[#344054] text-[14px]'>Card Scheme</span>
+        <div className='flex items-center gap-3'>
+          <Image src="/pics/left.png" alt='Card Icon' height={20} width={20} />
+          <p className='text-[#475467] text-[14px] font-medium'>Back</p>
+          <Image src="/pics/card.png" alt='Card Icon' height={16} width={16} />
+          <Image src="/pics/rig.png" alt='Card Icon' height={16} width={16} />
+          <span className='text-[#475467] text-[14px] font-medium'>Card Profile</span>
+          <Image src="/pics/rig.png" alt='Card Icon' height={16} width={16} />
+          <span className='text-[#001735] text-[14px] font-bold'>Create Profile</span>
         </div>
         <div className='flex items-center gap-4'>
-          <Image src="/pics/e.png" alt='Card Icon' height={16} width={16} />
-          <Image src="/pics/e.png" alt='Card Icon' height={16} width={16} />
+          <Image src="/pics/bell.png" alt='Card Icon' height={16} width={16} />
+          <Image src="/pics/drop.png" alt='Card Icon' height={32} width={32} />
         </div>
       </div>
       <div className='p-5 sm:flex sm:flex-col sm:gap-3'>
         <h1 className='text-[18px] text-[#101828] font-bold'>Card Request</h1>
         <span className='text-[#475467] text-[14px] font-normal'>View and attend to card requests here.</span>
       </div>
-      <div className='flex mx-5 flex-col gap-4 bg-[#FFFFFF] p-5 border-[#DEE6E2] border-[1px]'>
-        <h1 className='text-[18px] text-[#101828] font-bold'>Card Request</h1>
+      <div className='flex mx-5 flex-col gap-4 rounded-[10px] bg-[#FFFFFF] p-5 border-[#DEE6E2] border-[1px]'>
+        <h1 className='text-[18px] text-[#101828] font-bold'>Profile Details</h1>
         <div className='flex flex-col gap-4 sm:w-[100%]'>
           <div className='flex items-center gap-7 sm:flex sm:flex-col sm:gap-3'>
             <div className='flex flex-col gap-2 w-[448px] sm:w-[100%]'>
               <label className='text-[#344054] text-[14px]' htmlFor="">Branch Name</label>
-              <input type="text" placeholder='Corperate' className=' bg-[#FFFFFF] p-2 border-[#DEE6E2] border-[1px]' />
+              <input type="text" placeholder='Corperate' className=' bg-[#FFFFFF] p-2 border-[#DEE6E2] h-[44px] rounded-[8px] border-[1px]' />
             </div>
             <div className='flex flex-col gap-2 w-[448px] sm:w-[100%]'>
               <label  className='text-[#344054] text-[14px]' htmlFor="">Initiator</label>
-              <input type="text" placeholder='RootUser' className='text-[#101828] text-[16px] bg-[#FFFFFF] p-2 border-[#DEE6E2] border-[1px]'/>
+              <input type="text" placeholder='RootUser' className='text-[#101828] text-[16px] rounded-[8px]  h-[44px] bg-[#FFFFFF] p-2 border-[#DEE6E2] border-[1px]'/>
             </div>
           </div>
           <div className='flex items-center gap-7 sm:flex sm:flex-col sm:gap-3'>
             <div className='flex flex-col gap-2 w-[448px] sm:w-[100%]'>
               <label className='text-[#344054] text-[14px]' htmlFor="">Card Type</label>
-              <input type="text" placeholder='Classic Debit' className=' bg-[#FFFFFF] p-2 border-[#DEE6E2] border-[1px]' />
+              <input type="text" placeholder='Classic Debit' className=' bg-[#FFFFFF] p-2 border-[#DEE6E2] h-[44px] rounded-[8px] border-[1px]' />
             </div>
             <div className='flex flex-col gap-2 w-[448px] sm:w-[100%]'>
               <label  className='text-[#344054] text-[14px]' htmlFor="">Card Charges</label>
-              <input type="text" placeholder='₦1,500' className='text-[#101828] text-[16px] bg-[#FFFFFF] p-2 border-[#DEE6E2] border-[1px]'/>
+              <input type="text" placeholder='₦1,500' className='text-[#101828] text-[16px] bg-[#FFFFFF] h-[44px] rounded-[8px] p-2 border-[#DEE6E2] border-[1px]'/>
             </div>
           </div>
           <div className='flex items-center gap-7 sm:flex sm:flex-col sm:gap-3'>
             <div className='flex flex-col gap-2 w-[448px] sm:w-[100%]'>
               <label className='text-[#344054] text-[14px]' htmlFor="">Quantity</label>
-              <input type="text" placeholder='10' className=' bg-[#FFFFFF] p-2 border-[#DEE6E2] border-[1px]' />
+              <input type="text" placeholder='10' className=' bg-[#FFFFFF] p-2 border-[#DEE6E2] border-[1px] h-[44px] rounded-[8px]' />
             </div>
             <div className='flex flex-col gap-2 w-[448px] sm:w-[100%]'>
               <label  className='text-[#344054] text-[14px]' htmlFor="">Batch</label>
-              <input type="text" placeholder='847264905' className='text-[#101828] text-[16px] bg-[#FFFFFF] p-2 border-[#DEE6E2] border-[1px]'/>
+              <input type="text" placeholder='847264905' className='rounded-[8px] text-[#101828] text-[16px] h-[44px] bg-[#FFFFFF] p-2 border-[#DEE6E2] border-[1px]'/>
             </div>
           </div>
           <div className='flex items-center gap-7'>
             <div className='flex flex-col gap-2 w-[448px] sm:w-[100%]'>
               <label className='text-[#344054] text-[14px]' htmlFor="">Date Requested</label>
-              <input type="text" placeholder='Corperate' className=' bg-[#FFFFFF] p-2 border-[#DEE6E2] border-[1px]' />
+              <input type="text" placeholder='Corperate' className='rounded-[8px] bg-[#FFFFFF] p-2 h-[44px] border-[#DEE6E2] border-[1px]' />
             </div>
           </div>
         </div>
@@ -72,7 +77,7 @@ const CardUser = () => {
           {
             addFee && (
               <div className='fixed flex items-center justify-center w-[100%] h-[100vh] bg-[#101828] z-[90] top-0 left-0'>
-                <AddFee />
+                <AddFee setAddFee={setAddFee} />
               </div>
             )
           }
@@ -103,7 +108,7 @@ const CardUser = () => {
         </div>
       </div>
       <div className='sm:mx-5 sm:w-[full]'>
-        <button  className='m-[20px] w-[293px] h-[44px] bg-[#014DAF] text-[16px] text-[#FFFFFF] font-bold sm:w-[100%]'>
+        <button  className='rounded-[4px] m-[20px] w-[293px] h-[44px] bg-[#014DAF] text-[16px] text-[#FFFFFF] font-bold sm:w-[100%]'>
           Create Profile
         </button>
       </div>
--- src/app/components/AddFee.jsx
+++ src/app/components/AddFee.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const AddFee = ({setAddFee}) => {
   return (
-    <div className='bg-[#FFFFFF] w-[500px] flex flex-col gap-3'>
+    <div className='bg-[#FFFFFF] w-[500px] h-[] flex flex-col gap-1 pt-2 pb-2 rounded-[12px]'>
       <div className='mx-4 flex justify-between'>
         <div className='flex gap-2'>
           <img src="/pics/feature.png" alt="" />
@@ -17,13 +17,80 @@ const AddFee = ({setAddFee}) => {
         </button>
       </div>
       <hr className="w-[100%] h-[1px] bg-[#98A2B3]" />
-      <div className='mx-4'>
+      <div className='mx-4 flex flex-col gap-3'>
         <div className='flex flex-col gap-2'>
-          <label htmlFor="">Fee Name*</label>
-          <input type="text" placeholder='Maintenance' />
+          <label className='text-[#344054] text-[15px] font-medium' htmlFor="">Fee Name*</label>
+          <input className='w-[100%] border-[#D0D5DD] border-[1px] rounded-[8px] h-[44px] px-3 text-[16px] text-[#667085] font-normal' type="text" placeholder='Maintenance' />
+        </div>
+        <div className='flex flex-col gap-2'>
+          <label className='text-[#344054] text-[15px] font-medium' htmlFor="">Value*</label>
+          <input className='w-[100%] border-[#D0D5DD] border-[1px] rounded-[8px] h-[44px] px-3 text-[16px] text-[#667085] font-normal' type="number" placeholder='0' />
         </div>
       </div>
-      <div></div>
+      <div className='mx-4 flex flex-col gap-2'>
+        <label className='text-[#344054] text-[15px] font-medium' htmlFor="">Value*</label>
+        <div className='flex items-center gap-3'>
+          <div className='flex items-center gap-2'>
+            <input className='w-[24px] h-[24px]' type="radio" name="" id="" />
+            <label className='text-[#121212] text-[16px] font-normal' htmlFor="ngn">NGN</label>
+          </div>
+          <div className='flex items-center gap-2'>
+            <input className='w-[24px] h-[24px]' type="radio" name="" id="" />
+            <label className='text-[#121212] text-[16px] font-normal' htmlFor="usd">USD</label>
+          </div>
+        </div>
+      </div>
+      <div className='mx-4 flex flex-col gap-2'>
+        <label className='text-[#344054] text-[15px] font-medium' htmlFor="">Value*</label>
+        <div className='flex items-center gap-3'>
+          <div className='flex items-center gap-2'>
+            <input className='w-[24px] h-[24px]' type="radio" name="" id="" />
+            <label className='text-[#121212] text-[16px] font-normal' htmlFor="ngn">One Off</label>
+          </div>
+          <div className='flex items-center gap-2'>
+            <input className='w-[24px] h-[24px]' type="radio" name="" id="" />
+            <label className='text-[#121212] text-[16px] font-normal' htmlFor="usd">Monthy</label>
+          </div>
+        </div>
+      </div>
+      <div className='mx-4 flex flex-col gap-2'>
+        <label className='text-[#344054] text-[15px] font-medium' htmlFor="">Value*</label>
+        <div className='flex items-center gap-3'>
+          <div className='flex items-center gap-2'>
+            <input className='w-[24px] h-[24px]' type="radio" name="" id="" />
+            <label className='text-[#121212] text-[16px] font-normal' htmlFor="ngn">Issuance</label>
+          </div>
+          <div className='flex items-center gap-2'>
+            <input className='w-[24px] h-[24px]' type="radio" name="" id="" />
+            <label className='text-[#121212] text-[16px] font-normal' htmlFor="usd">Pin Reissue</label>
+          </div>
+        </div>
+      </div>
+      <div className='mx-4 flex flex-col gap-2'>
+        <label className='text-[#344054] text-[15px] font-medium' htmlFor="">Value*</label>
+        <div className='flex items-center gap-3'>
+          <div className='flex items-center gap-2'>
+            <input className='w-[24px] h-[24px]' type="radio" name="" id="" />
+            <label className='text-[#121212] text-[16px] font-normal' htmlFor="ngn">None</label>
+          </div>
+          <div className='flex items-center gap-2'>
+            <input className='w-[24px] h-[24px]' type="radio" name="" id="" />
+            <label className='text-[#121212] text-[16px] font-normal' htmlFor="usd">Branch Code Prefix</label>
+          </div>
+          <div className='flex items-center gap-2'>
+            <input className='w-[24px] h-[24px]' type="radio" name="" id="" />
+            <label className='text-[#121212] text-[16px] font-normal' htmlFor="usd">Branch Code Suffix</label>
+          </div>
+        </div>
+      </div>
+      <div className='mx-4 flex flex-col gap-2'>
+        <label className='text-[#344054] text-[15px] font-medium' htmlFor="">Value*</label>
+        <input className='w-[100%] border-[#D0D5DD] border-[1px] rounded-[8px] h-[44px] px-3 text-[16px] text-[#667085] font-normal' type="number" placeholder='Maintenance' />
+      </div>
+      <hr className="w-[100%] h-[1px] bg-[#98A2B3]" />
+      <div className='mx-4'>
+        <button className='bg-[#014DAF] w-[100%] h-[44px] rounded-[4px] text-[#FFFFFF] text-[16px] font-bold'>Add Fee</button>
+      </div>
     </div>
   )
 }
